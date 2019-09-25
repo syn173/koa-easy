886959afb0f8cc4c7b495ae879db9c208df4050e
大雾 <531506864@qq.com>
2019-09-25 11:02:23 +0800

    add proxy

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const Koa = require('koa');
 const logger = require('koa-logger');
 const server = require('koa-static');
 
+const proxy = require('http-proxy-middleware');
+const k2c = require('koa2-connect');
+
 const app = new Koa();
 
 //app.use(logger());
@@ -9,5 +12,21 @@ const app = new Koa();
 app.use(server(__dirname + '/src/assets'));
 
 app.use(server(__dirname));
+
+/* proxy api */
+app.use(async (ctx, next) => {  
+  if (ctx.url.startsWith('/api')) {   
+    ctx.respond = false;   
+    await k2c(proxy({    
+      target: 'http://mms.mengtuiapp.com',
+      changeOrigin: true,
+      secure: false,
+      // pathRewrite: {    
+      //   '^/api': '' 
+      // }    
+    }))(ctx, next);  
+  }  
+  await next()
+})
 
 app.listen(3000);
